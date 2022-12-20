6a05cc890006fc6b376d4d5750bc9969afd9b55e
nirzaf <mfmfazrin1986@gmail.com>
2022-12-20 10:44:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿#define Test
+#define TestImplement 
+
+using System.Diagnostics;
+using static System.Console;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TestImplement();
+        WriteLine("Hello World!");
+    }
+
+    [Conditional("Test123")]
+    static void Test()
+    {
+        WriteLine("Test");
+    }
+
+    [Conditional("TestImplement")]
+    static void TestImplement()
+    {
+        WriteLine("TestImplement");
+    }
+}
